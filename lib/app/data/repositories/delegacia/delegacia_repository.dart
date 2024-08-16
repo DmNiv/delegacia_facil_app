67a5f04884c60d498f801e844cbf2873082cc47c
Logan Cardoso <logan.cc@outlook.com>
2024-08-16 16:43:53 -0300

    fix: ajuste na chamada na API

--- lib/app/data/repositories/delegacia/delegacia_repository.dart
+++ lib/app/data/repositories/delegacia/delegacia_repository.dart
@@ -1,20 +1,24 @@
+import 'package:delegacia_facil_app/app/data/providers/delegacia_facil_api_client/delegacia_facil_api_client.provider.dart';
 import 'package:dio/dio.dart';
 import 'package:delegacia_facil_app/app/data/models/delegacia.model.dart';
 import 'package:delegacia_facil_app/app/data/repositories/delegacia/delegacia_repository.interface.dart';
 
 class DelegaciaRepository implements IDelegaciaRepository {
-  final Dio _dio;
+  final DelegaciaFacilApiClient apiClient;
 
-  DelegaciaRepository({Dio? dio}) : _dio = dio ?? Dio();
+  DelegaciaRepository(this.apiClient);
+
+  DelegaciaRepository.defaultClient() : apiClient = DelegaciaFacilApiClient();
 
   @override
   Future<List<Delegacia>> getDelegacias() async {
-    const url =
-        'https://9d7e-2804-14c-598f-89b1-2d79-a105-89a1-c93e.ngrok-free.app/delegacias';
-
+    const url = 'https://2284-2804-14c-598f-89b1-2d79-a105-89a1-c93e.ngrok-free.app/delegacias';
+    final Map<String, dynamic> headers = {'ngrok-skip-browser-warning': 1};
     try {
-      final response = await _dio.get(url);
-      final data = response.data as List;
+      final response = await apiClient.get(url, headers: headers);
+
+      final List<dynamic> data = response.data;
+
       return data.map((map) => Delegacia.fromMap(map)).toList();
     } catch (e) {
       throw Exception("Erro ao carregar as delegacias: $e");
