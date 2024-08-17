6b64325e4e1d84c836b60852fb7edbe32cd3b0f9
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2024-08-17 18:22:32 -0300

    fix: ngrok link

--- lib/app/data/repositories/delegacia/delegacia_repository.dart
+++ lib/app/data/repositories/delegacia/delegacia_repository.dart
@@ -12,12 +12,12 @@ class DelegaciaRepository implements IDelegaciaRepository {
   @override
   Future<List<Delegacia>> getDelegacias() async {
     const url =
-        'https://4f8a-177-100-88-202.ngrok-free.app/delegacias';
+        'https://0449-2804-14c-598f-89b1-1cd6-eaec-7c9d-f02b.ngrok-free.app/delegacias/lista-delegacias';
     final Map<String, dynamic> headers = {'ngrok-skip-browser-warning': 1};
     try {
       final response = await apiClient.get(url, headers: headers);
 
-      final List<dynamic> data = response.data;
+      final List<dynamic> data = response.data ?? [];
 
       return data.map((map) => Delegacia.fromMap(map)).toList();
     } catch (e) {
@@ -27,15 +27,14 @@ class DelegaciaRepository implements IDelegaciaRepository {
 
   @override
   Future<List<Delegacia>> getDelegacias24h(bool horario24h) async {
-    const baseUrl =
-        'https://4f8a-177-100-88-202.ngrok-free.app/delegacias';
-    final url = '$baseUrl/horario?horario24h=$horario24h';
+    const baseUrl = 'https://0449-2804-14c-598f-89b1-1cd6-eaec-7c9d-f02b.ngrok-free.app/delegacias';
+    final url = '$baseUrl/filtro?horario24h=$horario24h';
     final Map<String, dynamic> headers = {'ngrok-skip-browser-warning': 1};
 
     try {
       final response = await apiClient.get(url, headers: headers);
 
-      final List<dynamic> data = response.data;
+      final List<dynamic> data = response.data ?? [];
 
       return data.map((map) => Delegacia.fromMap(map)).toList();
     } catch (e) {
