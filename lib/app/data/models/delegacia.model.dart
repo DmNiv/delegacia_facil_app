fbc5c49fb6d48f59d89a63634be01412c6c0aa79
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2024-08-18 14:42:46 -0300

    fix: atualizando campos do modelo de delegacia

--- lib/app/data/models/delegacia.model.dart
+++ lib/app/data/models/delegacia.model.dart
@@ -2,17 +2,19 @@ class Delegacia {
   final String id;
   final String nome;
   final String endereco;
-  final bool horario24h;
+  final bool diaTodo;
   final double latitude;
   final double longitude;
+  final String mapUrl;
 
   Delegacia({
     required this.id,
     required this.nome,
     required this.endereco,
-    required this.horario24h,
+    required this.diaTodo,
     required this.latitude,
     required this.longitude,
+    required this.mapUrl
   });
 
   factory Delegacia.fromMap(Map<String, dynamic> map) {
@@ -20,9 +22,10 @@ class Delegacia {
       id: map['id'],
       nome: map['nome'],
       endereco: map['endereco'],
-      horario24h: map['horario24h'],
+      diaTodo: map['diaTodo'],
       latitude: map['latitude'],
       longitude: map['longitude'],
+      mapUrl: map['mapUrl']
     );
   }
 }
